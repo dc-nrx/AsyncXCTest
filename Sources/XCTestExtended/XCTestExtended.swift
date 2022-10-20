dd232c0f66ddd6d05c09dce6cf91b1ee0d602838
Dima Chapovskyi <dchapovskyi@shutterstock.com>
2022-10-20 19:09:28 +0100

    xeAsyncAssert in progress

--- Sources/XCTestExtended/XCTestExtended.swift
+++ Sources/XCTestExtended/XCTestExtended.swift
@@ -1,6 +1,28 @@
-public struct XCTestExtended {
-    public private(set) var text = "Hello, World!"
+import XCTest
 
-    public init() {
-    }
+extension XCTestCase {
+
+	public func xeAsyncAssert(
+		_ expression: @escaping @autoclosure () throws -> Bool,
+		_ message: @escaping @autoclosure () -> String = "",
+		timeout: TimeInterval = 3,
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) {
+		asyncTest(interval: timeout) {
+			XCTAssert(try expression(), message(), file: file, line: line)
+		}
+	}
+
+	private func asyncTest(
+		interval: TimeInterval = 3,
+		test: @escaping () throws -> ()
+	) {
+		let expectation = expectation(description: "Wait for \(interval) seconds")
+		DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+			try! test()
+			expectation.fulfill()
+		}
+		waitForExpectations(timeout: interval + 0.2)
+	}
 }
