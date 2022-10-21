a2d64dbee124d66b7510b9ebdbec0b7ad72478eb
Dima Chapovskyi <dchapovskyi@shutterstock.com>
2022-10-21 14:33:15 +0100

    waitUntil implemented / xeAsyncAssert test

--- Sources/XCTestExtended/XCTestExtended.swift
+++ Sources/XCTestExtended/XCTestExtended.swift
@@ -1,11 +1,17 @@
 import XCTest
 
+public struct Defaults {
+
+	public static let asyncTimeout: TimeInterval = 3
+
+}
+
 extension XCTestCase {
 
 	public func xeAsyncAssert(
 		_ expression: @escaping @autoclosure () throws -> Bool,
 		_ message: @escaping @autoclosure () -> String = "",
-		timeout: TimeInterval = 3,
+		timeout: TimeInterval = Defaults.asyncTimeout,
 		file: StaticString = #filePath,
 		line: UInt = #line
 	) {
@@ -17,4 +23,14 @@ extension XCTestCase {
 		waitForExpectations(timeout: timeout + 0.2)
 	}
 
+	public func waitUntil(
+		timeout: TimeInterval = Defaults.asyncTimeout,
+		file: StaticString = #file,
+		line: UInt = #line,
+		action: @escaping (@escaping () -> Void) -> Void
+	) {
+		let expectation = expectation(description: "Wait for \(timeout) seconds")
+		action(expectation.fulfill)
+		waitForExpectations(timeout: timeout)
+	}
 }
--- Tests/XCTestExtendedTests/XCTestExtendedTests.swift
+++ Tests/XCTestExtendedTests/XCTestExtendedTests.swift
@@ -1,10 +1,20 @@
 import XCTest
 @testable import XCTestExtended
 
-final class XCTestExtendedTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+final class AsyncAssertSpec: XCTestCase {
+
+	func testExample() throws {
+        var i = 1
+		DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+			self.change(&i, to: 2)
+		}
+		xeAsyncAssert(i == 2)
     }
+
+	private func change<T>(
+		_ value: inout T,
+		to: T
+	) {
+		value = to
+	}
 }
