8d261b55bb90546b56a1af2f7581db22af4ea764
Dima Chapovskyi <dchapovskyi@shutterstock.com>
2022-10-24 14:01:50 +0100

    repeat implemented (no tests yet)

--- Sources/XCTestExtended/XCTestExtended.swift
+++ Sources/XCTestExtended/XCTestExtended.swift
@@ -3,6 +3,7 @@ import XCTest
 public struct Defaults {
 
 	public static let asyncTimeout: TimeInterval = 3
+	public static let asyncRepeatFrequency = 0.1
 	public static let waitForExpectationExtraDuration = 0.1
 
 }
@@ -19,11 +20,21 @@ extension XCTestCase {
 		_ expression: @escaping @autoclosure () throws -> Bool,
 		_ message: @escaping @autoclosure () -> String = "",
 		timeout: TimeInterval = Defaults.asyncTimeout,
-		repeatFrequency: TimeInterval? = nil,
+		repeatFrequency: TimeInterval? = Defaults.asyncRepeatFrequency,
 		file: StaticString = #filePath,
 		line: UInt = #line
 	) {
 		let expectation = expectation(description: "Wait for \(timeout) seconds")
+		// Repeated evaluations
+		if let repeatFrequency = repeatFrequency,
+		   repeatFrequency < timeout {
+			let _ = Timer(fire: Date() + repeatFrequency, interval: repeatFrequency, repeats: true) { _ in
+				if (try? expression()) == true {
+					expectation.fulfill()
+				}
+			}
+		}
+		// Evaluation after timeout
 		DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
 			try! XCTAssert(expression(), message(), file: file, line: line)
 			expectation.fulfill()
@@ -31,15 +42,19 @@ extension XCTestCase {
 		waitForExpectations(timeout: timeout + Defaults.waitForExpectationExtraDuration)
 	}
 
+	/**
+	 A handy wrapper for an expectation
+	 */
 	public func waitUntil(
 		timeout: TimeInterval = Defaults.asyncTimeout,
-		file: StaticString = #file,
-		line: UInt = #line,
+//		file: StaticString = #file,
+//		line: UInt = #line,
 		action: @escaping (@escaping () -> Void) -> Void
 	) {
 		let expectation = expectation(description: "Wait for \(timeout) seconds")
 		action(expectation.fulfill)
 		waitForExpectations(timeout: timeout)
+		
 	}
 
 	public func fail(
