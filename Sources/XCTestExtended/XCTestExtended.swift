8d78679a65fb2f656dff7839ae30ab8e62e01f45
Dima Chapovskyi <dchapovskyi@shutterstock.com>
2022-10-24 12:07:14 +0100

    refactor / documentation

--- Sources/XCTestExtended/XCTestExtended.swift
+++ Sources/XCTestExtended/XCTestExtended.swift
@@ -3,15 +3,23 @@ import XCTest
 public struct Defaults {
 
 	public static let asyncTimeout: TimeInterval = 3
+	public static let waitForExpectationExtraDuration = 0.1
 
 }
 
 extension XCTestCase {
 
+	/**
+	 A proxy method to execute `XCTAssert` after given `timeout`. Parameters are forwarded to `XCTAssert` call as is.
+	 - Parameter timeout: How long to wait until `expression` becomes `true`.
+	 - Parameter repeatFrequency: Frequency of `expression` (re)evaluation. Used to reduce waiting duration.
+	 If `nil`, `expression` will be evaluated just once after `timeout` have passed.
+	 */
 	public func xeAsyncAssert(
 		_ expression: @escaping @autoclosure () throws -> Bool,
 		_ message: @escaping @autoclosure () -> String = "",
 		timeout: TimeInterval = Defaults.asyncTimeout,
+		repeatFrequency: TimeInterval? = nil,
 		file: StaticString = #filePath,
 		line: UInt = #line
 	) {
@@ -20,7 +28,7 @@ extension XCTestCase {
 			try! XCTAssert(expression(), message(), file: file, line: line)
 			expectation.fulfill()
 		}
-		waitForExpectations(timeout: timeout + 0.2)
+		waitForExpectations(timeout: timeout + Defaults.waitForExpectationExtraDuration)
 	}
 
 	public func waitUntil(
@@ -32,5 +40,12 @@ extension XCTestCase {
 		let expectation = expectation(description: "Wait for \(timeout) seconds")
 		action(expectation.fulfill)
 		waitForExpectations(timeout: timeout)
+	}
+
+	public func fail(
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) {
+		XCTFail(file: file, line: line)
 	}
 }
