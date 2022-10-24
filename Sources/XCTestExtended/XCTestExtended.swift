07ee32c1968e8fd1500290f2b9cff165f54bf0ee
Dima Chapovskyi <dchapovskyi@shutterstock.com>
2022-10-24 20:33:35 +0100

    xeAsyncAssertEqual, xeAsyncAssertTrue implemeted

--- Sources/XCTestExtended/XCTestExtended.swift
+++ Sources/XCTestExtended/XCTestExtended.swift
@@ -5,8 +5,6 @@ extension XCTestCase {
 	/**
 	 A proxy method to execute `XCTAssert` after given `timeout`. Parameters are forwarded to `XCTAssert` call as is.
 	 - Parameter timeout: How long to wait until `expression` becomes `true`.
-	 - Parameter repeatFrequency: Frequency of `expression` (re)evaluation. Used to reduce waiting duration.
-	 If `nil`, `expression` will be evaluated just once after `timeout` have passed.
 	 */
 	public func xeAsyncAssert(
 		_ expression: @escaping @autoclosure () throws -> Bool,
@@ -21,17 +19,39 @@ extension XCTestCase {
 		}
 	}
 
-	private func asyncAssertScheduler(
-		timeout: TimeInterval,
-		condition: @escaping @autoclosure () throws -> Bool,
-		assertClosure: @escaping () throws -> ()
+	/**
+	 A proxy method to execute `XCTAssertEqual` after given `timeout`. Parameters are forwarded to `XCTAssert` call as is.
+	 - Parameter timeout: How long to wait until `expression` becomes `true`.
+	 */
+	public func xeAsyncAssertEqual<T: Equatable>(
+		_ expression1: @escaping @autoclosure () throws -> T,
+		_ expression2: @escaping @autoclosure () throws -> T,
+		_ message: @escaping @autoclosure () -> String = "",
+		timeout: TimeInterval = Defaults.asyncTimeout,
+		repeatFrequency: TimeInterval? = Defaults.asyncRepeatFrequency,
+		file: StaticString = #filePath,
+		line: UInt = #line
 	) {
-		let expectation = expectation(description: "Wait for \(timeout) seconds")
-		DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
-			try! assertClosure()
-			expectation.fulfill()
+		asyncAssertScheduler(timeout: timeout, condition: try expression1() == expression2()) {
+			XCTAssertEqual(try expression1(), try expression2(), message(), file: file, line: line)
 		}
-		waitForExpectations(timeout: timeout + Defaults.waitForExpectationExtraDuration)
+	}
+
+	/**
+	 A proxy method to execute `XCTAssertEqual` after given `timeout`. Parameters are forwarded to `XCTAssert` call as is.
+	 - Parameter timeout: How long to wait until `expression` becomes `true`.
+	 */
+	public func xeAsyncAssertTrue(
+		_ expression: @escaping @autoclosure () throws -> Bool,
+		_ message: @escaping @autoclosure () -> String = "",
+		timeout: TimeInterval = Defaults.asyncTimeout,
+		repeatFrequency: TimeInterval? = Defaults.asyncRepeatFrequency,
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) {
+		asyncAssertScheduler(timeout: timeout, condition: try expression()) {
+			XCTAssertTrue(try expression(), message(), file: file, line: line)
+		}
 	}
 
 	/**
@@ -53,4 +73,18 @@ extension XCTestCase {
 	) {
 		XCTFail(file: file, line: line)
 	}
+
+	private func asyncAssertScheduler(
+		timeout: TimeInterval,
+		condition: @escaping @autoclosure () throws -> Bool,
+		assertClosure: @escaping () throws -> ()
+	) {
+		let expectation = expectation(description: "Wait for \(timeout) seconds")
+		DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
+			try! assertClosure()
+			expectation.fulfill()
+		}
+		waitForExpectations(timeout: timeout + Defaults.waitForExpectationExtraDuration)
+	}
+
 }
